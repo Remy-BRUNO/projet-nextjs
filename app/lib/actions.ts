bb0601e630cc3ff5dbf64100ef08df82ab1c4de7
Reiggnar <bruno.remy.sebastien@gmail.com>
2023-11-13 12:14:50 +0100

    add auth/next

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -105,3 +105,20 @@ export async function deleteInvoice(id: string) {
   }
   revalidatePath("/dashboard/invoices")
 }
+
+// authentification
+import { signIn } from "@/auth"
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", Object.fromEntries(formData))
+  } catch (error) {
+    if ((error as Error).message.includes("CredentialsSignin")) {
+      return "CredentialSignin"
+    }
+    throw error
+  }
+}
